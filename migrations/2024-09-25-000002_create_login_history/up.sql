6999db8a1140faa9463e73634aa4fa7735dc3b76
princenizroh <113965138+princenizroh@users.noreply.github.com>
2024-09-25 16:46:30 +0800

    Update up.sql

--- migrations/2024-09-25-000002_create_login_history/up.sql
+++ migrations/2024-09-25-000002_create_login_history/up.sql
@@ -3,13 +3,23 @@ CREATE TABLE login_history (
     login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     logout_time TIMESTAMP,
     login_data VARCHAR(255),
-    player_id INT REFERENCES Player(player_id) ON DELETE CASCADE
+    player_id INT REFERENCES players(player_id) ON DELETE CASCADE
 );
 
-INSERT INTO login_history (player_id, login_data, login_time)
-VALUES 
-(1, 'Logged in via Web', '2024-09-25 10:00:00'),
-(2, 'Logged in via Mobile', '2024-09-25 11:30:00'),
-(3, 'Logged in via Web', '2024-09-25 12:45:00');
+-- Stored Procedure untuk menambah data ke tabel login_histories
+CREATE OR REPLACE PROCEDURE add_player_history(p_player_id INT, p_login_data VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO login_history (player_id, login_data)
+    VALUES (p_player_id, p_login_data);
+END;
+$$;
 
+
+-- Data awal
+INSERT INTO login_history (player_id, login_data)
+VALUES 
+(1, 'Logged in via Android'),
+(2, 'Logged in via Web');
 
